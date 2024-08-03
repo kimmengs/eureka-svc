79d20f086677584aa653f09e050e887f7d2ba935
Kimmeng <kimmeng.work@gmail.com>
2024-08-03 21:11:31 +0700

    inital model

---
+++ src/test/java/com/aditi/eureka_svc/EurekaSvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aditi.eureka_svc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaSvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
